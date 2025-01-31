68f3d70a286c011b8124ccf3af54ec9e538d7d5e
rajkishortiwari9794 <rajkishortiwari9794@gmail.com>
2025-02-01 03:23:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 28660719
-api_hash = "34ade52147ebcaf4bc0b88aa3dfc7e1a"
-bot_token = "7001245667:AAGm3VkNigvYgO5nrB9Civflz9lop0B91d8"
-auth_users = "7460749492"
+api_id = 280719
+api_hash = "34ade52147ebbc0b88aa3dfc7e1a"
+bot_token = "7001245667:AAGNigvYgO5nrB9Civflz9lop0B91d8"
+auth_users = "74749492"
 sudo_users = [int(num) for num in auth_users.split("100")]
 osowner_users = "7460749492"
 owner_users = [int(num) for num in osowner_users.split("100")]
